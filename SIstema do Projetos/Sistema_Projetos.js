706a8aea6a6a0589d838d2a491e7358c09cbf410
João Queiroz Almeida <44407401+joaoqueir0zq@users.noreply.github.com>
2018-11-29 18:40:32 -0300

    Add files via upload

---
+++ SIstema do Projetos/Sistema_Projetos.js
@@ -0,0 +1,16 @@
+function home()
+{
+	window.location.href="Sistema_Projetos.html"
+}
+function sobre()
+{
+	window.location.href="container_1.html"
+}
+function projetos()
+{
+	window.location.href="container_2.html"
+}
+function orientadores()
+{
+	window.location.href="container_3.html"
+}
